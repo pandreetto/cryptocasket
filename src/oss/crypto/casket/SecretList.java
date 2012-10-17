85c5b3b9fcf6129000b195efddac3f82393c03d6
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2012-10-17 13:02:47 +0200

    Skeleton for the secret list

---
+++ src/oss/crypto/casket/SecretList.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) Paolo Andreetto
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+
+package oss.crypto.casket;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+public class SecretList extends ListActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        String[] mStrings = new String[20];
+        for (int k=0; k<20; k++) {
+            mStrings[k] = "Test " + k;
+        }
+        
+        setListAdapter(new ArrayAdapter<String>(this,
+                R.layout.secretitem, mStrings));
+        getListView().setTextFilterEnabled(true);
+
+        
+    }
+
+}
